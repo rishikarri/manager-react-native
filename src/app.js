291eec18700d5fd204a8f7b6339cf9fbe086183b
Rishi Karri <rishikarri1@gmail.com>
2017-04-18 23:03:11 -0400

    learned how to turn off specific errors in eslint

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers/';
+import LoginForm from './components/LoginForm.js'
 
 // before the component renders for the first time (compnentwillmount), configure firebase 
 
@@ -23,11 +24,7 @@ class App extends Component {
 	render() {
 		return (
 			<Provider store={createStore(reducers)}>
-				<View>
-					<Text>
-						Hello 
-					</Text>
-				</View>
+				<LoginForm />
 			</Provider>
 		);
 	}
---
+++ src/components/LoginForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Card, CardSection, Input, Button } from './common/index.js';
+
+class LoginForm extends Component {
+	onEmailChange(text) {
+		
+	}
+	render() {
+		return (
+			<Card>
+				<CardSection>
+					<Input 
+						label="Email"
+						placeholder="email@gmail.com"
+						onChangeText={this.onEmailChange.bind(this)}
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Input
+						secureTextEntry
+						label="Password"
+						placeholder="password"
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Button> 
+						Login
+					</Button>
+				</CardSection>
+			</Card>
+		);
+	}
+}
+
+export default LoginForm;
