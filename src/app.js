1c580e1818f3bc7994e160bdc3ecef599a523903
Rishi Karri <rishikarri1@gmail.com>
2017-04-18 22:39:36 -0400

    added common components

--- src/app.js
+++ src/app.js
@@ -2,21 +2,35 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers/';
 
-
+// before the component renders for the first time (compnentwillmount), configure firebase 
 
 class App extends Component {
+	componentWillMount() {
+		const config = {
+			apiKey: 'AIzaSyD5EUG_C4s2utoGB36Sj5rjGc5kLHTPaLc',
+			authDomain: 'manager-402ed.firebaseapp.com',
+			databaseURL: 'https://manager-402ed.firebaseio.com',
+			projectId: 'manager-402ed',
+			storageBucket: 'manager-402ed.appspot.com',
+			messagingSenderId: '744964124023'
+			};
+			firebase.initializeApp(config);
+	}
+
 	render() {
 		return (
-			<Provider store={createStore()}>
+			<Provider store={createStore(reducers)}>
 				<View>
 					<Text>
 						Hello 
 					</Text>
 				</View>
 			</Provider>
-		)
+		);
 	}
 }
 
-export default App; 
+export default App;
